ede181625fc4d8e40700ad50689ceeaf5a843fca
lvxiaopeng <lvxiaopeng@outlook.com>
2020-04-24 17:51:24 +0800

    引入资源

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import Elementul from 'element-ui' // 引入ui
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styless/index.less' // 引入初始化样式
 Vue.config.productionTip = false
-
+Vue.use(Elementul)
 new Vue({
   router,
   render: h => h(App)

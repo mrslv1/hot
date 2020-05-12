c1201fcc5e66e3a8dc2ed8807425c15e70e9a42b
lvxiaopeng <lvxiaopeng@outlook.com>
2020-05-12 15:18:12 +0800

    项目初始化-新增主页和登录页

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import Elementul from 'element-ui' // 引入ui
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import './styless/index.less' // 引入初始化样式
+import Video from 'video.js'
+import 'video.js/dist/video-js.css'
+Vue.prototype.$video = Video
 Vue.config.productionTip = false
 Vue.use(Elementul)
 new Vue({
